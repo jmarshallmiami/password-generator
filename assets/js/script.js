9567602fbf6654534d6924d0b7ca29cd96b5cf5f
Jules <j.marshallmiami@yahoo.com>
2022-03-19 11:34:03 -0500

    generate password function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
 
+function generatePassword(){
+// prompt user to for password lenght between 1-128 characters
+var characterLength = prompt("Choose a character length for your password between 1-128 characters:");
+
+
+// prompt user to include lowerCase letter in password 
+var addLowerCase = confirm("Your password will have lowercase letters.")
+
+// prompt user to include upperCase letter in password 
+var addUpperCase = confirm("Your password will have uppercase letters.")
+
+// prompt user to include lowerCase letter in password 
+var addNumbers = confirm("Your password will have numeric values.")
+
+// prompt user to include lowerCase letter in password 
+var addSpecialChar = confirm("Your password will have special characters.")
+
+
+
+ };
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
